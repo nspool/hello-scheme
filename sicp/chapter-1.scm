6e7a1059889caab225659604f793e6b81d799946
nspool <nspool@gmx.com>
2017-10-30 19:15:21 +1100

    WIP: Testing for primality

--- sicp/chapter-1.scm
+++ sicp/chapter-1.scm
@@ -96,4 +96,13 @@
       a
       (my-gcd b (remainder a b))))
 
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+
 
